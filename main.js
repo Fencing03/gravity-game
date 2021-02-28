6857298a680757fbfaa1d1b4c9d13533ba0b9957
Paul Ronz <pierre@pc-25.home>
2021-02-28 12:04:26 +0100

    fith commit

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ var game = new Phaser.Game(config)
 if (this.game.device.desktop){            
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;            
     this.scale.minWidth = gameWidth/2;            
-    this.scale.minHeight = gameHeight/2;            
+    this.scale.minHeight = gameHeight/2.5;            
     this.scale.maxWidth = gameWidth;            
     this.scale.maxHeight = gameHeight;            
     this.scale.pageAlignHorizontally = true;            
@@ -41,7 +41,7 @@ if (this.game.device.desktop){
     else       
      { this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;            
         this.scale.minWidth = gameWidth/2;            
-        this.scale.minHeight = gameHeight/2;            
+        this.scale.minHeight = gameHeight/2.5;            
         this.scale.maxWidth = 1035; //You can change this to gameWidth*2.5 if needed            this.scale.maxHeight = 1228; //Make sure these values are proportional to the gameWidth and gameHeight            this.scale.pageAlignHorizontally = true;            this.scale.pageAlignVertically = true;            this.scale.forceOrientation(true, false);            this.scale.hasResized.add(this.gameResized, this);            this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);            this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);            this.scale.setScreenSize(true);        }
      }
 var ow = parseInt(this.game.canvas.style.width,10);var oh = parseInt(this.game.canvas.style.height,10);var r = Math.max(window.innerWidth/ow,window.innerHeight/oh);var nw = ow*r;var nh = oh*r;this.game.canvas.style.width = nw+"px";this.game.canvas.style.height= nh+"px";this.game.canvas.style.marginLeft = (window.innerWidth/2 - nw/2)+"px"; this.game.canvas.style.marginTop = (window.innerHeight/2 - nh/2)+"px";document.getElementById("game").style.width = window.innerWidth+"px";document.getElementById("game").style.height = window.innerHeight-1+"px";//The css for body includes 1px top margin, I believe this is the cause for this -1document.getElementById("game").style.overflow = "hidden";
